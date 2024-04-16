e5f997eda650ac5e84e679789de101c10745961f
Melika Dabiri <jalehdabiri@gmail.com>
2024-04-15 21:13:16 -0700

    find user by name as query param

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,9 +40,23 @@ app.get("/", (req, res) => {
     res.send("hello world!");
 })
 
+
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
 app.get("/users", (req, res) => {
-    res.send(users);
-  });
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+});
 
 app.listen(port, () => {
     console.log(
