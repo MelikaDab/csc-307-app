15147f6d49a298a4ad5d805ea8195edb5df574cc
Melika Dabiri <jalehdabiri@gmail.com>
2024-04-16 00:53:40 -0700

    req params - find user by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,18 +34,23 @@ const users = {
         job: "Bartender"
       }
     ]
-  };
+};
 
-app.get("/", (req, res) => {
-    res.send("hello world!");
-})
-
+// helper functions
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 const findUserByName = (name) => {
     return users["users_list"].filter(
-      (user) => user["name"] === name
+    (user) => user["name"] === name
     );
-  };
+};
+
+
+// routes
+app.get("/", (req, res) => {
+    res.send("hello world!");
+})
   
 app.get("/users", (req, res) => {
     const name = req.query.name;
@@ -56,6 +61,16 @@ app.get("/users", (req, res) => {
     } else {
       res.send(users);
     }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
