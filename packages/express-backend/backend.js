f6646f1e61fce5f057e298270b0f156b8f54d437
Melika Dabiri <jalehdabiri@gmail.com>
2024-04-15 20:55:47 -0700

    users endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,9 +6,43 @@ const port = 8000;
 // set up our express app to process incoming data in json format
 app.use(express.json());
 
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
 app.get("/", (req, res) => {
     res.send("hello world!");
 })
+
+app.get("/users", (req, res) => {
+    res.send(users);
+  });
 
 app.listen(port, () => {
     console.log(
