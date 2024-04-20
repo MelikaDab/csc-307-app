e66b29f80ddb1cc7d3d991a69f41fb742e6bdfb3
Melika Dabiri <jalehdabiri@gmail.com>
2024-04-19 22:25:17 -0700

    delete button deletes on backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,31 +7,45 @@ function MyApp() {
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
-      .then((json) => { 
-        setCharacters(json["users_list"]); 
+      .then((json) => {
+        setCharacters(json["users_list"]);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
+  function removeOneCharacter(userId) {
+    console.log(userId);
+    const url = `http://localhost:8000/users/${userId}`;
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.status === 204) {
+          const updated = characters.filter(
+            (character) => character.id !== userId
+          );
+          setCharacters(updated);
+        } else {
+          console.log("Failed to delete user");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   function updateList(person) {
     postUser(person)
       .then(async (res) => {
         if (res.status === 201) {
-          const newCharacter = await res.json(); 
+          const newCharacter = await res.json();
           setCharacters([...characters, newCharacter]);
         } else {
           console.log("Create unsuccessful");
         }
-        })
+      })
       .catch((error) => {
         console.log(error);
       });
